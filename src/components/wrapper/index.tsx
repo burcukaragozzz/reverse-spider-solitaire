8b5d41945668a997836cbbb87cf1bddce551b9c5
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-08-16 19:54:08 +0300

    delete wrapper conponent

--- src/components/wrapper/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Container } from './styled';
-
-export const Wrapper: React.FC = ({ children }) => {
-    return <Container>{children}</Container>;
-};
--- src/components/wrapper/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/macro";
-
-export const Container = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  height:100%;
-  background: ${({ theme }) => theme.colors.background};
-`;
