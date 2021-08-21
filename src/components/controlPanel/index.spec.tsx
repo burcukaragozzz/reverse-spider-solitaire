8c57426bec09f5035737fcbcff22d98e850a4069
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-08-21 19:50:41 +0300

    add modal test in control panel component

--- src/components/controlPanel/index.spec.tsx
+++ src/components/controlPanel/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from 'test';
+import { fireEvent, render } from 'test';
 
 import { ControlPanel } from './index';
 
@@ -9,24 +9,38 @@ describe('ControlPanel', () => {
         expect(component).toBeTruthy();
     });
 
-    it('renders back button successfully', () => {
-        const { getByText } = render(<ControlPanel />);
-
-        getByText('Back Home');
-    });
-
-    it('renders score text successfully', () => {
-        const { getByText } = render(<ControlPanel />);
-
-        getByText('Score');
-    });
-
     it('renders 3 icons successfully', () => {
         const { container } = render(<ControlPanel />);
 
         const icons = container.querySelectorAll('img');
 
         expect(icons).toHaveLength(3);
+    });
+
+    it('renders back button successfully', () => {
+        const { getByText } = render(<ControlPanel />);
+
+        getByText('Back Home');
+    });
+
+    xit('modal', () => {
+        const mockFn = jest.fn();
+
+        const { getByText } = render(<ControlPanel />);
+
+        const backBtn = getByText('Back Home');
+
+        fireEvent.click(backBtn);
+
+        expect(mockFn).toHaveBeenCalledTimes(1);
+
+        getByText('If you come back the game will be over. Are you sure want to come back?');
+    });
+
+    it('renders score text successfully', () => {
+        const { getByText } = render(<ControlPanel />);
+
+        getByText('Score :');
     });
 
     it('renders Timer successfully', () => {
