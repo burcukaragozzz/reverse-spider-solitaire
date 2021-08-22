304effed3ce403fa2433a3ce81f36ef9885ab9da
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-08-22 10:27:46 +0300

    update cypress settings

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,1 +1,16 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import './commands';
+
+/// <reference types="cypress" />
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Custom command to select DOM element by selector.
+             * @example cy.getBySel('greeting')
+             */
+            getBySel(target: string): Chainable<Element>;
+        }
+    }
+}
